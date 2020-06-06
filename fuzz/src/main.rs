5360560faa4ed1546cfed270bbee0cd4187eccbf
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-06-06 11:44:23 +0000

    add singleton

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -1,4 +1,8 @@
 use crypto_api_chachapoly::{ ChachaPolyIetf, XChachaPoly, crypto_api::cipher::AeadCipher };
 use sodiumoxide::crypto::{
     stream::salsa20,
- 
+    aead::{ chacha20poly1305_ietf, xchacha20poly1305_ietf }
+};
+use hex::ToHex;
+use std::{
+    env, thread, ops::Range, 
