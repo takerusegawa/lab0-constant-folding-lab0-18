545ea54d8d9fb189b51053b8058c561e64e92df8
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-08-04 08:47:32 +0000

    Code cleaned

--- src/chachapoly_ietf.rs
+++ src/chachapoly_ietf.rs
@@ -1,3 +1,9 @@
 use crate::{ ChachaPolyError, ChaCha20Ietf, Poly1305 };
 use crypto_api::{
-    cipher:
+    cipher::{ CipherInfo, Cipher, AeadCipher },
+    rng::{ SecureRng, SecKeyGen }
+};
+use std::error::Error;
+
+
+/// The m
