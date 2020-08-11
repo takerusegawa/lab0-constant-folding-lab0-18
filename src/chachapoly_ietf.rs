239a1b7dc6278ac38f679b3ed8e246d6b1f4d327
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-08-11 10:14:37 +0000

    Updated summary.

--- src/chachapoly_ietf.rs
+++ src/chachapoly_ietf.rs
@@ -11,4 +11,6 @@ use std::error::Error;
 pub const CHACHAPOLY_MAX: usize = (4_294_967_296 - 1) * 64; // (2^32 - 1) * BLOCK_SIZE
 /// The maximum amount of bytes that can be processed with one key/nonce combination
 #[cfg(target_pointer_width = "32")]
-pub c
+pub const CHACHAPOLY_MAX: usize = usize::max_value() - 16; // 2^32 - 1 - 16
+
+/// The size of a ChaChaPoly k
