be03b4c0e80caf84d66bd3bf64fdef772ba931c2
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-08-22 11:06:06 +0000

    add

--- src/chachapoly_ietf.rs
+++ src/chachapoly_ietf.rs
@@ -47,4 +47,5 @@ pub fn chachapoly_open(data: &mut[u8], tag: &[u8], ad: &[u8], key: &[u8], nonce:
     
     // Compute the Poly1305 key and the authentication tag
     let (mut pkey, mut vfy_tag) = (vec![0; 32], vec![0; 16]);
-    Ch
+    ChaCha20Ietf::xor(key, nonce, 0, &mut pkey);
+    Poly1305::chachapoly_auth(&mut vfy_tag, ad, data, &foot,
