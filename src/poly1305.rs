75af75f6345d77622b71bc58ce463d4da6605a90
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-10-28 16:39:32 +0000

    Fix after merge

--- src/poly1305.rs
+++ src/poly1305.rs
@@ -1,3 +1,9 @@
 use crate::core::poly1305::{ poly1305_init, poly1305_update, poly1305_finish };
 use crypto_api::{
-    mac::{ MacInfo, Ma
+    mac::{ MacInfo, Mac },
+    rng::{ SecureRng, SecKeyGen }
+};
+use std::error::Error;
+
+
+/// The size of a Poly1305 k
