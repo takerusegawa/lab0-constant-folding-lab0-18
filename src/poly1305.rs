22fead2627a6b01104e0e115eb378645b9fa1362
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-10-29 05:54:11 +0000

    Update LICENSE and NOTICE

--- src/poly1305.rs
+++ src/poly1305.rs
@@ -6,4 +6,7 @@ use crypto_api::{
 use std::error::Error;
 
 
-/// The size of a Poly1305 k
+/// The size of a Poly1305 key (256 bits/32 bytes)
+pub const POLY1305_KEY: usize = 32;
+/// The size of a ChaChaPoly authentication tag
+pub cons
