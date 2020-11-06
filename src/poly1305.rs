538b0c0fabdfedd5061e1c36ec9f1c295ab6ccee
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-11-06 13:52:21 +0000

    Release v0.5

--- src/poly1305.rs
+++ src/poly1305.rs
@@ -30,4 +30,8 @@ impl Poly1305 {
         poly1305_update(&mut a, &r, &u, ad, false);
         poly1305_update(&mut a, &r, &u, data, false);
         poly1305_update(&mut a, &r, &u, foot, true);
-        poly1305_finish(tag, &mut a, &mut s
+        poly1305_finish(tag, &mut a, &mut s);
+    }
+}
+impl SecKeyGen for Poly1305 {
+    fn new_sec_key(&self, buf: &mut[u8], rng: &mut dyn SecureRng) ->
