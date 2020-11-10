416e014e077a3bc7b7601c88237acfd819b32dab
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2020-11-10 10:18:42 +0000

    add fontawesome

--- src/poly1305.rs
+++ src/poly1305.rs
@@ -36,4 +36,8 @@ impl Poly1305 {
 impl SecKeyGen for Poly1305 {
     fn new_sec_key(&self, buf: &mut[u8], rng: &mut dyn SecureRng) -> Result<usize, Box<dyn Error + 'static>> {
         // Verify input
-        vfy_keygen!(POLY1305_KEY => bu
+        vfy_keygen!(POLY1305_KEY => buf);
+        
+        // Generate key
+        rng.random(&mut buf[..POLY1305_KEY])?;
+   
