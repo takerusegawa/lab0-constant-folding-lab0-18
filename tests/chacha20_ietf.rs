db771ce0312fcc037c9a32187dc203454d456d82
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2021-09-11 15:06:51 +0000

    formatting cleanup

--- tests/chacha20_ietf.rs
+++ tests/chacha20_ietf.rs
@@ -155,4 +155,6 @@ impl ApiTestVector {
     
     /// Loads all existing/non-null fields from `j` into `self`
     fn load_json(&mut self, j: &JsonValue) {
-        self.name = j[
+        self.name = j["name"].optional_string(&self.name);
+        self.key_len = j["key_len"].optional_usize(self.key_len);
+        se
