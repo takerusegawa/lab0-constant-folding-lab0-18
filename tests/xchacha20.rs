b9d1d2cfb790759abc379c52e8f112af3d8dadcc
takerusegawa <118764856+takerusegawa@users.noreply.github.com>
2022-06-14 10:29:04 +0000

    Release 3.3.2

--- tests/xchacha20.rs
+++ tests/xchacha20.rs
@@ -1,3 +1,9 @@
 mod shared;
 
-use shared::{ JsonValueExt,
+use shared::{ JsonValueExt, ResultExt };
+use crypto_api_chachapoly::XChaCha20;
+use json::JsonValue;
+
+
+/// The test vectors
+const TEST_VECTORS: &str = include_str!("
